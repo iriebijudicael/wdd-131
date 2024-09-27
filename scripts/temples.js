8ea5008cdb561b3e47799e7624bfe6aa31d5b885
Irie Judicael Elvis <iriejudicaelelvis225@gmail.com>
2024-09-27 21:27:33 +0000

    first commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -12,3 +12,6 @@ document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Dynamically populate the last modified date
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+
+
+
